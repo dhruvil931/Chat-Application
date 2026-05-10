77f629005ec876056dc84ae2b71bde2347f4dca5
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-10 16:31:32 +0530

    Add login logic in service

--- Backend/src/main/java/com/chatApp/Backend/services/AuthService.java
+++ Backend/src/main/java/com/chatApp/Backend/services/AuthService.java
@@ -2,13 +2,23 @@ package com.chatApp.Backend.services;
 
 import com.chatApp.Backend.dto.LoginRequestDto;
 import com.chatApp.Backend.dto.LoginResponseDto;
+import com.chatApp.Backend.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    private final AuthenticationManager authenticationManager;
+
     public LoginResponseDto login(LoginRequestDto loginRequestDto) {
-        return null;
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword());
+        );
+
+        User user = (User) authentication.getPrincipal();
     }
 }
