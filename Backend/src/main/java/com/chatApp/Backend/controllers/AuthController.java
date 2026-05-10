f0a31c07e6abca7d77758b339f3fbf5766d4ae88
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-10 16:22:04 +0530

    Updates AuthController

--- Backend/src/main/java/com/chatApp/Backend/controllers/AuthController.java
+++ Backend/src/main/java/com/chatApp/Backend/controllers/AuthController.java
@@ -1,7 +1,12 @@
 package com.chatApp.Backend.controllers;
 
+import com.chatApp.Backend.dto.LoginRequestDto;
+import com.chatApp.Backend.dto.LoginResponseDto;
 import com.chatApp.Backend.services.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
-
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseEntity.ok(authService.login(loginRequestDto);
+    }
 }
