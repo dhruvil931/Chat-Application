547cf18c057f5eadd7b99dbca43fbeed4a63bd13
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-11 10:05:52 +0530

    Add extractUsername from token

--- Backend/src/main/java/com/chatApp/Backend/services/JwtService.java
+++ Backend/src/main/java/com/chatApp/Backend/services/JwtService.java
@@ -1,5 +1,6 @@
 package com.chatApp.Backend.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -30,5 +31,15 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    private String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
     }
 }
