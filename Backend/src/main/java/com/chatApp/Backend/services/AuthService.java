40e4d68abf2402552f1609b1114eb4e025d9b350
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-11 10:31:58 +0530

    Added register in AuthService

--- Backend/src/main/java/com/chatApp/Backend/services/AuthService.java
+++ Backend/src/main/java/com/chatApp/Backend/services/AuthService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,7 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
     public LoginResponseDto login(LoginRequestDto loginRequestDto) {
         Authentication authentication = authenticationManager.authenticate(
@@ -29,5 +31,20 @@ public class AuthService {
         String token = jwtService.generateToken(user.getUsername());
 
         return new LoginResponseDto(token, username);
+    }
+
+    public void register(LoginRequestDto loginRequestDto) {
+        boolean exists = userRepository.findByUsername(loginRequestDto.getUsername()).isPresent();
+
+        if(exists) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        User user = User.builder()
+                .username(loginRequestDto.getUsername())
+                .password(passwordEncoder.encode(loginRequestDto.getPassword()))
+                .build();
+
+        userRepository.save(user);
     }
 }
