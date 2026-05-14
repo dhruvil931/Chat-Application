dcfed39b1433def869210723d6590ca2b9e5986b
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-14 11:52:21 +0530

    Change file to right folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import JoinCreateChat from "./components/JoinCreateChat";
+import JoinCreateChat from "./pages/JoinCreateChat";
 
 function App() {
   return (
