d0c57bd553cc9210160325e349dafc146e574ac5
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-17 13:58:57 +0530

    Added backend real url

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://chat-application-i1b9.onrender.com";
 
 export const httpClient = axios.create({
   baseURL,
