af57739ebe489c0e5bc1f56f877568280130440f
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-17 14:06:27 +0530

    Added frontend real api

--- Backend/src/main/java/com/chatApp/Backend/config/AppConstants.java
+++ Backend/src/main/java/com/chatApp/Backend/config/AppConstants.java
@@ -1,5 +1,5 @@
 package com.chatApp.Backend.config;
 
 public class AppConstants {
-    public static final String FRONTEND_BASED_URL="http://localhost:5173";
+    public static final String FRONTEND_BASED_URL="https://chat-application-bay-mu.vercel.app/";
 }
