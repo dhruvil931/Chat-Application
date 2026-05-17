937bb2d8c48f60053796fa40471267fe7bfb7776
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-17 16:59:07 +0530

    Add MongoDB URI check and update application properties for improved logging

--- Backend/src/main/java/com/chatApp/Backend/config/AppConfig.java
+++ Backend/src/main/java/com/chatApp/Backend/config/AppConfig.java
@@ -1,11 +1,16 @@
 package com.chatApp.Backend.config;
 
+import jakarta.annotation.PostConstruct;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    @PostConstruct
+    public void checkMongoUri() {
+        System.out.println("MONGO URI = " + System.getenv("MONGO_URI"));
+    }
 
     @Bean
     public ModelMapper modelMapper() {
