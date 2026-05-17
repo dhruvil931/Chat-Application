d33a6684d9e264732c24d85939123f8769e0513a
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-17 15:24:22 +0530

    Correction

--- Backend/src/main/java/com/chatApp/Backend/config/AppConstants.java
+++ Backend/src/main/java/com/chatApp/Backend/config/AppConstants.java
@@ -1,5 +1,5 @@
 package com.chatApp.Backend.config;
 
 public class AppConstants {
-    public static final String FRONTEND_BASED_URL="https://chat-application-bay-mu.vercel.app/";
+    public static final String FRONTEND_BASED_URL="https://chat-application-bay-mu.vercel.app";
 }
