a5ba03b02a4265097617254644211dfb992aece7
Dhruvil Kapadiya <dhruvil.k1087@gmail.com>
2026-05-17 18:39:33 +0530

    Log PORT environment variable in main method

--- Backend/src/main/java/com/chatApp/Backend/BackendApplication.java
+++ Backend/src/main/java/com/chatApp/Backend/BackendApplication.java
@@ -8,6 +8,7 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		System.out.println(">>> MONGO_URI = " + System.getenv("MONGO_URI"));
+		System.out.println(">>> PORT = " + System.getenv("PORT"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
